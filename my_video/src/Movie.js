fd0f2a79be2e957179e74c8c6eda51b57404e0a3
Caleb Norris <45391626+CalebN99@users.noreply.github.com>
2021-04-09 13:50:19 -0700

    Streaming service api start

--- my_video/src/Movie.js
+++ my_video/src/Movie.js
@@ -36,7 +36,7 @@ handleVideoLoad = (e) => {
         <div className="movieBox">
         <ReactPlayer
               
-              url={this.state.videoFileURL}
+              url="http://localhost:3030/video"
               width='100%'
               height='100%'
               controls = {true}
@@ -48,8 +48,6 @@ handleVideoLoad = (e) => {
       </div>
     );
   }
- 
-
 }
 
 export default Movie;
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.get('/movies', function (req, res) {
+app.get('/', function (req, res) {
     res.send('hello world')
   })
 
   app.get('/video', function(req, res) {
-    const path = 'assets/sample.mp4'
+    const path = 'Movies\\Napolean_Dynamite.mp4'
     const stat = fs.statSync(path)
     const fileSize = stat.size
     const range = req.headers.range
