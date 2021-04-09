ab03fdc4c53bdb280266729d87c42df19c25dc80
Caleb Norris <45391626+CalebN99@users.noreply.github.com>
2021-04-09 11:55:34 -0700

    Video player working

--- my_video/src/Movie.js
+++ my_video/src/Movie.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./Movie.css";
+import ReactPlayer from "react-player";
 
-function Movie() {
-  return (
-    <div className="Movie">
-      <div className="movieBox"></div>
-      <h2 className="movieTitle">Us</h2>
-    </div>
-  );
+class Movie extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+     videoFileURL: '',
+     videoFileObject: null
+    };
+  };
+
+handleChange = (e) => {
+    this.setState({
+        [e.target.id] : e.target.value
+    })
+}
+handleVideoLoad = (e) => {
+  console.log(e.target.files);
+  let files = e.target.files;
+  if (files.length === 1) {
+    let file = files[0];
+    this.setState({
+      videoFileURL: URL.createObjectURL(file),
+      videoFileObject: file
+    });
+  }
+  console.log(this.state.videoFileObject);
+}
+  render() {
+    console.log("YO")
+    return (
+      <div className="Movie">
+        <div className="movieBox">
+        <ReactPlayer
+              
+              url={this.state.videoFileURL}
+              width='100%'
+              height='100%'
+              controls = {true}
+  
+              />
+        </div>
+        <h2 className="movieTitle">Us</h2>
+        <input type="file" id="file" onChange={this.handleVideoLoad} accept="video/mp4,video/x-m4v,video/*"/>
+      </div>
+    );
+  }
+ 
+
 }
 
 export default Movie;
